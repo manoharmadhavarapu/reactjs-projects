8a255836d3d38e9ebb659e840829c26810660c2d
manoharmadhavarapu <manohar37nec@gmail.com>
2024-07-02 12:39:33 +0530

    random-color-generator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
       <Accordian/>
+
+      {/* Random Color Component */}
+      <RandomColor/>
+
     </div>
   );
 }
